f0b04a8da5caaad29266f7b2de3fd4fe0e2b548c
Andy Blum <andy@gravityworksdesign.com>
2020-09-07 15:30:02 -0400

    fetch players in parallel chunks

--- build.js
+++ build.js
@@ -2,10 +2,6 @@ const fetch = require('node-fetch')
 const writeJsonFile = require('write-json-file');
 const getObj = require('json-from-text');
 
-const promises = {
-  'rosters': [],
-  'players': []
-};
 
 const key = /window\[\'\_\_espnfitt\_\_\'\]/;
 
@@ -44,7 +40,6 @@ const key = /window\[\'\_\_espnfitt\_\_\'\]/;
         if (payload[key].hasOwnProperty('page')) {
           let posgroups = payload[key].page.content.roster.groups;
           for (const group of posgroups) {
-            await writeJsonFile('./posgroups.json', group);
             for (const player of group.athletes) {
               ids.push(player.id);
             }
@@ -55,45 +50,68 @@ const key = /window\[\'\_\_espnfitt\_\_\'\]/;
   });
   await writeJsonFile('./NFLPlayerIDs.json', ids);
 
-  // Fetch all player info
+  // Create blocks of 25 ids to fetch in parallel
   const players = [];
-  for (const id of ids) {
-    console.log(`Player ${players.length + 1} of ${ids.length}`);
-    const payload = await fetch(`https://www.espn.com/nfl/player/_/id/${id}`)
-      .then(resp => resp.text())
-      .then(text => getObj(text).jsonResults)
-      .catch(err => console.log(err));
+  const chunks = [];
 
-    for (const key in payload) {
-      if (payload[key].hasOwnProperty('page')) {
-        let data = payload[key].page.content.player;
+  const chunkSize = 10
+  const chunkCount = Math.ceil(ids.length / chunkSize);
+  const idsLength = ids.length;
 
-        const ath = data.prtlCmnApiRsp.athlete;
-        const header = data.plyrHdr.ath
-        const player = {
-          'age': ath.age,
-          'dob': ath.displayDOB,
-          'firstName': header.fNm,
-          'id': ath.playerId,
-          'img': header.img,
-          'lastName': header.lNm,
-          'logo': header.logo,
-          'num': header.dspNum,
-          'pos': header.posAbv,
-          'status': ath.abbreviation,
-          'team': ath.team.abbrev,
-        };
-
-        if (!!ath.displayDraft) {
-          player.draft = ath.displayDraft.substr(0, 4)
-        }
-
-        players.push(player);
-        if (players.length % 50 === 0) {
-          await writeJsonFile('./NFLPlayers.json', players);
-        }
+  for (let chunkIndex = 0; chunkIndex < chunkCount; chunkIndex++) {
+    const chunk = [];
+    for (let index = 0; index < chunkSize; index++) {
+      if (ids[0]) {
+        chunk.push(ids.shift());
       }
     }
+    chunks.push(chunk);
   }
-  await writeJsonFile('./NFLPlayers.json', players);
+
+  // Fetch players
+  for (const chunk of chunks) {
+    let promises = [];
+
+    for (const id of chunk) {
+      promises.push(fetch(`https://www.espn.com/nfl/player/_/id/${id}`)
+        .then(resp => resp.text()))
+    }
+
+    await Promise.all(promises).then(async (vals) => {
+      for (const val of vals) {
+        const payload = getObj(val).jsonResults;
+        for (const key in payload) {
+          if (payload[key].hasOwnProperty('page')) {
+            let data = payload[key].page.content.player;
+
+            const ath = data.prtlCmnApiRsp.athlete;
+            const header = data.plyrHdr.ath
+            const player = {
+              'age': ath.age,
+              'dob': ath.displayDOB,
+              'firstName': header.fNm,
+              'id': ath.playerId,
+              'img': header.img,
+              'lastName': header.lNm,
+              'logo': header.logo,
+              'num': header.dspNum,
+              'pos': header.posAbv,
+              'status': ath.abbreviation,
+              'team': ath.team.abbrev,
+            };
+
+            if (!!ath.displayDraft) {
+              player.draft = ath.displayDraft.substr(0, 4)
+            }
+
+            players.push(player);
+          }
+        }
+      }
+    });
+
+    await writeJsonFile('./NFLPlayers.json', players);
+    console.log(`${players.length}/${idsLength}`);
+  }
+
 })();
